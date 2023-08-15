4dfc1959e5d7d83a6b643b8ed0f3f4e820467c38
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2023-08-15 22:52:03 +0530

    Create Display longest name.cpp

---
+++ Display longest name.cpp
@@ -0,0 +1,58 @@
+Example:
+
+Input:
+N = 5
+names[] = { "Geek", "Geeks", "Geeksfor",
+  "GeeksforGeek", "GeeksforGeeks" }
+
+Output:
+GeeksforGeeks
+
+  class Solution{
+    public:
+    string longest(string names[], int n)
+    {   int v[n];
+         
+        for(int i=0;i<n;i++)
+        {
+            
+            v[i]=names[i].length();
+        }
+        
+       int max=v[0];
+       int index=0;
+       for(int i=0;i<n;i++)
+       {
+           if(max<v[i])
+           {
+               max=v[i];
+               index=i;
+               
+           }
+       }
+       return names[index];
+      
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
